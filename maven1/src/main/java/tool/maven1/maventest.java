2d496542bf33bcb9c2e4686b41ed1ea913b9da8d
bharg <bharg@DESKTOP-9JHJ27R>
2021-05-06 17:11:41 +0530

    Commiting the project

---
+++ maven1/src/main/java/tool/maven1/maventest.java
@@ -0,0 +1,23 @@
+ package tool.maven1;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class maventest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("bhargav");
+		System.setProperty("webdriver.chrome.driver", "D:\\Selenium\\chromedriver.exe");
+		
+		WebDriver driver= new ChromeDriver();
+			
+		driver.get("https://www.google.com/");
+			
+		driver.close();
+
+
+	}
+
+}
